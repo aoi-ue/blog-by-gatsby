73a3b58e6809aab61f716d0eddf7f2df33462a41
yuhanyuhan <yuhanlian@gmail.com>
2020-02-01 17:05:35 +0800

    Setting up main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,31 @@
 import React from "react"
 import { Link } from "gatsby"
+import Button from "@material-ui/core/Button"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const Component = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const StyledButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+`
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Component>
+      <Link to="/page-2/">
+        <StyledButton> New Request </StyledButton>
+      </Link>
+    </Component>
+  )
+}
 
 export default IndexPage
